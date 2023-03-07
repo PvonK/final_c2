f832381606bfacd1f814a7fba06ea7dd1c7af32b
PvonK <philippvonkesselstatt@gmail.com>
2023-03-07 09:19:36 -0300

    added ip and file clearing arguments

--- argument_definition.py
+++ argument_definition.py
@@ -5,11 +5,17 @@ def argument_definition():
 
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("-i", "--ip", default="127.0.0.1",
+                        help="ip where server is setup")
+
     parser.add_argument("-p", "--port", type=int, default=5000,
-                        help="port where server gets setup")
+                        help="port where server is setup")
 
     parser.add_argument("-d", "--directory", default="/ex_files",
                         help="server's file storage directory")
+
+    parser.add_argument("-f", "--fresh", action="store_true",
+                        help="clean server storage if it exists")
 
     args = parser.parse_args()
 
--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 from argument_definition import argument_definition
 from routes import getRoutes
+import shutil
 
 from aiohttp import web
 import os
 
 
-def server_setup(port, directory):
-    HOST, PORT = "127.0.0.1", port
+def server_setup(ip, port, directory):
+    HOST, PORT = ip, port
 
     app = web.Application()
     app.router.add_routes(getRoutes(directory))
-    web.run_app(app, port=PORT, host=HOST, reuse_address=True)
-
+    try:
+        web.run_app(app, port=PORT, host=HOST, reuse_address=True)
+    except OSError:
+        print("Server could not start")
 
 def main():
     args = argument_definition()
 
     newpath = os.path.dirname(__file__)+"/"+args.directory
 
+    if os.path.exists(newpath) and args.fresh:
+        shutil.rmtree(newpath)
+
+
     if not os.path.exists(newpath):
         os.makedirs(newpath)
 
-    server_setup(args.port, "/" + args.directory + "/")
+    server_setup(args.ip, args.port, "/" + args.directory + "/")
 
 
 if __name__ == "__main__":
